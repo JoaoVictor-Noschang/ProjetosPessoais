d8c0862e511202167599a48344f38241fe10bff1
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2024-04-28 12:42:06 -0400

    update relogio
    Adicionada função que muda a cor de fundo de acordo com o horário do dia

--- relogio/js/main.js
+++ relogio/js/main.js
@@ -8,7 +8,17 @@ setInterval( () => {
     let newMinutes = fulldate.getMinutes();
     let newSeconds = fulldate.getSeconds();
 
+    colorBackground(newHours);
+
     hours.textContent = newHours;
     minutes.textContent = newMinutes;
     seconds.textContent = newSeconds;
 }, 1000);
+
+function colorBackground(hours){
+    if (hours < 18) {
+        document.body.style.backgroundColor = "#d1c68a";
+    } else {
+        document.body.style.backgroundColor = "#1d1d1d";
+    }
+}
