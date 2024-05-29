f08de68fa9fcf8829fb923ff7696cc3108bf37c1
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2024-05-28 23:09:52 -0400

    refatorando crud
    refatorando novamente o crud de lotes de produtos.

--- crud_02/scripts/clear.js
+++
@@ -1,9 +0,0 @@
-function clear() {
-    document.getElementById('produto').value = '';
-    document.getElementById('fornecedor').value = '';
-    document.getElementById('lote').value = '';
-    document.getElementById('quantidade').value = '';
-    document.getElementById('vencimento').value = '';
-}
-
-export default clear;
---
+++ crud_02/scripts/index.js
@@ -0,0 +1,149 @@
+const cadastros = [];
+let cod = cadastros.length > 0 ? cadastros[cadastros.length - 1].id : 0;
+
+const codigoInput = document.querySelector("#codigo");
+const produtoInput = document.querySelector("#produto");
+const fornecedorInput = document.querySelector("#fornecedor");
+const loteInput = document.querySelector("#lote");
+const quantidadeInput = document.querySelector("#quantidade");
+const vencimentoInput = document.querySelector("#vencimento");
+
+const actionButton = document.querySelector("#submit-button");
+
+function Lote(produto, fornecedor, lote, quantidade, vencimento) {
+    this.id = ++cod;
+    this.produto = produto;
+    this.fornecedor = fornecedor;
+    this.lote = lote;
+    this.quantidade = quantidade;
+    this.vencimento = vencimento;
+}
+
+let lote01 = new Lote('Cerveja Local', 'Ambev', '31225-00015', '200', '31-12-2024');
+cadastros.push(lote01);
+
+exibirDados();
+verificarAcao();
+
+
+function exibirDados() {
+    let row = '<table><thead><tr>' +
+        '<th>Excluir</th>' +
+        '<th>Editar</th>' +
+        '<th>Código</th>' +
+        '<th>Produto</th>' +
+        '<th>Fornecedor</th>' +
+        '<th>Lote</th>' +
+        '<th>Quantidade</th>' +
+        '<th>Vencimento</th>'+
+        '</tr></thead><tbody>';
+
+    for (let i in cadastros) {
+
+        row += `<tr><td><img onclick='excluirCadastro(${cadastros[i].id})' src='assets/delete.svg' alt='Edit'></td>` +
+            `<td><img onclick='getDadosInInput(${cadastros[i].id})' src='assets/edit.svg' alt='Edit' ></td>` +
+            `<td>${cadastros[i].id}</td>` +
+            `<td>${cadastros[i].produto}</td>` +
+            `<td>${cadastros[i].fornecedor}</td>` +
+            `<td>${cadastros[i].lote}</td>` +
+            `<td>${cadastros[i].quantidade}</td>` +
+            `<td>${cadastros[i].vencimento}</td></tr>`;
+
+    }
+
+    row += '</tbody></table>';
+
+    document.getElementById('resultados').innerHTML = row;
+}
+
+
+function verificarAcao() {
+    actionButton.textContent = codigoInput.value ? "Atualizar" : "Cadastrar";
+
+    actionButton.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        if (codigoInput.value) {
+            atualizarLote();
+
+        } else {
+            const newRegistro = cadastrarLote();
+
+            if (newRegistro) {
+                cadastros.push(newRegistro);
+                exibirDados();
+                clear();
+
+            } else {
+                alert("Todos os dados devem ser informados!");
+            }
+        }
+    }, { once: true });
+}
+
+function clear() {
+    codigoInput.value = '';
+    produtoInput.value = '';
+    fornecedorInput.value = '';
+    loteInput.value = '';
+    quantidadeInput.value = '';
+    vencimentoInput.value = '';
+}
+
+function excluirCadastro(produtoId) {
+    for (let i in cadastros) {
+        if (cadastros[i].id == produtoId) {
+            cadastros.splice(i, 1);
+            alert('Registro deletado');
+        }
+    }
+    exibirDados();
+    clear();
+}
+
+function cadastrarLote() {
+
+    const newProduto = produtoInput.value;
+    const newFornecedor = fornecedorInput.value;
+    const newLote = loteInput.value;
+    const newQuantidade = quantidadeInput.value;
+    const newVencimento = vencimentoInput.value;
+
+
+    if (newProduto && newFornecedor && newLote && newQuantidade && newVencimento) {
+        return new Lote(newProduto, newFornecedor, newLote, newQuantidade, newVencimento);
+    } else {
+        return false;
+    }
+
+}
+
+function getDadosInInput(loteId) {
+    for (let i in cadastros) {
+        if (cadastros[i].id == loteId) {
+            codigoInput.value = cadastros[i].id;
+            produtoInput.value = cadastros[i].produto;
+            fornecedorInput.value = cadastros[i].fornecedor;
+            loteInput.value = cadastros[i].lote;
+            quantidadeInput.value = cadastros[i].quantidade;
+            vencimentoInput.value = cadastros[i].vencimento;
+            break;
+        }
+    }
+}
+
+function atualizarLote() {
+    const loteId = parseInt(codigoInput.value);
+    const lote = cadastros.find(l => l.id === loteId);
+    if (lote) {
+        lote.produto = produtoInput.value;
+        lote.fornecedor = fornecedorInput.value;
+        lote.lote = loteInput.value;
+        lote.quantidade = quantidadeInput.value;
+        lote.vencimento = vencimentoInput.value;
+        exibirDados();
+        clear();
+    } else {
+        alert("Lote não encontrado!");
+    }
+}
